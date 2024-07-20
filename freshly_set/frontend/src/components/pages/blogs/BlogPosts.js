0c31bdda72885b66649a852e26b42ced2475aab1
unknown <allanmathenge2.com>
2024-07-20 17:38:26 +0300

    minor stylings

--- freshly_set/frontend/src/components/pages/blogs/BlogPosts.js
+++ freshly_set/frontend/src/components/pages/blogs/BlogPosts.js
@@ -1,52 +1,55 @@
-// import { useState } from 'react'
-import { IoCaretDownSharp } from "react-icons/io5";
+import { useState } from 'react'
+import { IoCaretDownSharp, IoCaretUpSharp } from "react-icons/io5";
 import { FaThumbsUp } from "react-icons/fa6";
 import { MdMessage } from "react-icons/md";
 import { BsShareFill } from "react-icons/bs";
 
-const BlogPosts = () => {
+const BlogPosts = ({ post }) => {
 
-    // const [toggle, setToggle] = useState(1);
+    const [toggle, setToggle] = useState(null);
 
-    // const toggleTab = (index) => {
-    //     setToggle(index)
-    // }
+    const toggleTab = (index) => {
+        setToggle(index)
+    }
 
     return (
         <>
-            <section className="text-base sm:text-xl">
-                <div className="w-[96%] border-solid border-[1px] border-gray-200 mx-auto rounded-lg shadow-sm">
-                    <div className="">
+            <section className="text-base sm:text-xl text-gray-900">
+                <div className="w-[96%] border-solid border-[1px] bg-gray-100 border-gray-200 mx-auto rounded-lg shadow-md">
+                    <div>
 
                         <div className="flex w-[96%] mx-auto justify-between items-center">
-                            <h4 className="text-wrap font-medium m-1">Freshly Farms New Products Release</h4>
-                            <IoCaretDownSharp className="cursor-pointer" />
+                            <h4 className="text-wrap font-medium m-1">{post.title}</h4>
+                            {
+                                toggle
+                                 ? <IoCaretUpSharp className="cursor-pointer hover:text-gray-700" onClick={() => toggleTab(null)} />
+                                 : <IoCaretDownSharp onClick={() => toggleTab(post.id) } className="cursor-pointer hover:text-gray-700" />
+                            }
                         </div>
 
-                        <div className={"flex justify-around items-start sm:items-center sm:h-full overflow-hidden"}>
+                        <div className={toggle ? "flex justify-around items-start sm:items-center sm:h-full overflow-hidden transition" : "hidden"}>
 
                             <div className="flex flex-col max-w-[48%] h-[240px] sm:h-full overflow-hidden p-3">
 
                                 <div>
-                                    <h6 className="m-1 text-sm font-thin">By Freshly Farms, on 07 Sept, 2024</h6>
+                                    <h6 className="m-1 text-sm font-thin">{post.date}</h6>
                                 </div>
 
-                                <div className="w-full sm:h-full overflow-hidden overflow-y-auto text-gray-600">
+                                <div className="w-full sm:h-full overflow-hidden overflow-y-auto scrollbar scrollbar-track-gray-100 ">
                                     <p className="">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
-                                    Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                    {post.body}
                                     </p>
                                 </div>
 
-                                <div className="sm:w-[48%] flex justify-between text-2xl w-full ">
-                                    <p><FaThumbsUp /></p>
-                                    <p><MdMessage /></p>
-                                    <p><BsShareFill /></p>
+                                <div className="sm:w-[48%] flex justify-between text-2xl w-full">
+                                    <p><FaThumbsUp className="cursor-pointer hover:text-blue-700 transition duration-300" /></p>
+                                    <p><MdMessage className="cursor-pointer hover:text-blue-700 transition duration-300" /></p>
+                                    <p><BsShareFill className="cursor-pointer hover:text-blue-700 transition duration-300" /></p>
                                 </div>
                             </div>
 
                             <div className="w-[72%] sm:max-w-[40%] md:max-w-[32%] lg:max-w-[24%] ">
-                                <img className="w-full" src="/static/media/blogsImg.png" alt="freshly farms blog img" />
+                                <img className="w-full" src={post.image} alt={post.title} />
                             </div>
 
                         </div>
--- freshly_set/frontend/src/components/pages/blogs/Blogs.js
+++ freshly_set/frontend/src/components/pages/blogs/Blogs.js
@@ -1,14 +1,44 @@
+import { useEffect, useState } from 'react'
 import BlogPosts from './BlogPosts'
+import api from '../../../api/blogs'
 
 const Blogs = () => {
 
+  const [blogs, setBlogs] = useState([])
+
+ useEffect(() => {
+
+  const fetchBlogs = async () => {
+    try {
+      const response = await api.get('/blogs');
+      setBlogs(response.data);
+
+    } catch (error) {
+      if (error) {
+        // Catch errors out of 200 range
+        console.log(error.response.data)
+        console.log(error.response.headers)
+        console.log(error.response.status)
+      } else {
+        // any other errors within 200
+        console.log(error.response.message)
+      }
+    }
+  }
+  fetchBlogs()
+ }, [])
+
+//  console.log(blogs)
   return (
-    <div>
-      <h1 className="text-center text-6xl text-green-800">What's new?</h1>
-        <div className="flex flex-col gap-8">
-          <BlogPosts />
-          <BlogPosts />
-          <BlogPosts />
+    <div className="bg-slate-100 min-h-[100vh] py-8">
+      <h1 className="text-center text-6xl text-green-800 m-0">What's new?</h1>
+        <div className="flex flex-col gap-8 py-8 max-w-[88%] sm:w-full mx-auto">
+          {
+            blogs ? blogs.map((blog) => {
+              return <BlogPosts key={blog.id} post={blog} />
+            })
+            : <h4>Loading ... </h4>
+          }
       </div>
     </div>
   );
