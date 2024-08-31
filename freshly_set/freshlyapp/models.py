2a7730be2cc0b51648eb00ac97aecf7c316068a2
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-08-31 11:56:41 +0300

    create the order db schema

--- freshly_set/freshlyapp/models.py
+++ freshly_set/freshlyapp/models.py
@@ -278,17 +278,19 @@ class IDVerification(models.Model):
             return response['FaceMatches'][0]['Similarity'] > 80
 
 
-# order models(Product, Category, Cart, Orders)
+# order models(Product, Category, Orders)
 #  category model
 class Category(models.Model):
     CATEGORY_CHOICES = [
-        ('fruits','Fruits'),
-        ('vegetables','Vegetables'),
-        ('legumes','Legumes'),
-        ('spices','Spices'),
+        ('FR','Fruits'),
+        ('VG','Vegetables'),
+        ('LG','Legumes'),
+        ('SP','Spices'),
     ]
 
-    name = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default='FR')
+    name = models.CharField(max_length=200,
+                            choices=CATEGORY_CHOICES,
+                            default='FR')
     # slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
@@ -336,7 +338,42 @@ class Product(models.Model):
     #     return reverse('views:product_detail',
     #                 args=[self.id,self.slug])
 
-
-# cart models
 # order models
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    # address = models.CharField(max_length=250)
+    # postal_code = models.CharField(max_length=20)
+    # city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['-created']
+        indexes = [ models.Index(fields=['-created']),]
+
+    def __str__(self):
+        return f'Order {self.id}'
+    
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey( Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def get_cost(self):
+        return self.price * self.quantity
