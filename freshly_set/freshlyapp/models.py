08ecf23d61fe965269c0b2e77967cb274a40d0d6
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-09-02 11:25:30 +0300

    create a database schema for orders
    desing and implement the database schema for the orders endpoint. This will encompass 4 tables namely products, categories, orders and orderitems

--- freshly_set/freshlyapp/models.py
+++ freshly_set/freshlyapp/models.py
@@ -8,6 +8,8 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.urls import reverse
 import boto3
 import re
 
@@ -85,16 +87,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
-
-    def __str__(self):
-        return self.name
 
 
 class Garden(models.Model):
@@ -303,3 +295,105 @@ class IDVerification(models.Model):
             return True
         
         return False
+    
+
+# order models(Product, Category, Orders)
+#  category model
+class Category(models.Model):
+    CATEGORY_CHOICES = [
+        ('FR','Fruits'),
+        ('VG','Vegetables'),
+        ('LG','Legumes'),
+        ('SP','Spices'),
+    ]
+
+    name = models.CharField(max_length=200,
+                            choices=CATEGORY_CHOICES,
+                            default='FR')
+    # slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('views:product_list_by_category',)
+                    #    args=[self.slug])
+
+
+# products model
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id','slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created_at'])
+        ]
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('views:product_detail',
+    #                 args=[self.id,self.slug])
+
+# order models
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    # address = models.CharField(max_length=250)
+    # postal_code = models.CharField(max_length=20)
+    # city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [ models.Index(fields=['-created']),]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey( Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
