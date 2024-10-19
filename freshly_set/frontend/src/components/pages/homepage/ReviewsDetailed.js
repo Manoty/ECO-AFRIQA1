a0f95a9a5c43022ada6c65d557cab8b8e66430ca
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-10-19 16:48:10 +0300

    The updated detailed reviews section of the homepage

--- freshly_set/frontend/src/components/pages/homepage/ReviewsDetailed.js
+++ freshly_set/frontend/src/components/pages/homepage/ReviewsDetailed.js
@@ -6,7 +6,7 @@ import ReviewsDetailedData from './json/ReviewsDetailedData.json'
 
 function ReviewsDetailed() {
   // Number of Reviewers to display per page
-  const ReviewersNumber = 10;
+  const ReviewersNumber = 6;
 
   return (
     <div className="ReviewersDetailed">
@@ -15,10 +15,9 @@ function ReviewsDetailed() {
       </div>
 
       {/*InnerWrapper */}
-      <div className='InnerWrapper mx-[6px] lg:mx-[20px] mt-[150px] lg:mt-[200px] lg:pb-[50px]'>
+      <div className='InnerWrapper mx-[12px] lg:mx-[20px] mt-[150px] lg:mt-[200px] pb-[30px] lg:pb-[50px]'>
         {/*Back Button */}
-        
-          <div className="BackButton block w-[61px] h-[47px]">   
+          <div className="BackButton block w-[35px] lg:w-[61px] h-[27px] lg:h-[47px]">   
             <Link to="/">
               <img src="/static/media/image10.png" alt="Return back" className="w-full h-full" />
             </Link>
@@ -26,11 +25,11 @@ function ReviewsDetailed() {
 
         {/*The Page Title */}
         <div className='ReviewersTitle'>
-          <h4 className="text-[#008000] lg:text-[35px] text-center font-[700] font-inter my-0">Our Reviews</h4>
+          <h4 className="text-[#008000] text-[25px] lg:text-[35px] text-center font-[700] font-inter my-0">Our Reviews</h4>
         </div>
 
         {/* Reviwers Cards */}
-        <div className='ReviewersCards lg:mx-[90px]'>
+        <div className='ReviewersCards mt-[10px] mx-[20px] lg:mx-[90px]'>
           {ReviewsDetailedData.slice(0, ReviewersNumber).map((ReviewsDetailedData) =>
           <ReviewsDetailedCard  img={ReviewsDetailedData.img} name={ReviewsDetailedData.name} role={ReviewsDetailedData.role} reviews={ReviewsDetailedData.reviews} ratings={ReviewsDetailedData.ratings}/>
           )}
