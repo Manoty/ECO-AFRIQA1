d60b8298fa02fad4d1463bd84b8d7e7494f2bdef
Stedoh <stevensimoni55@gmail.com>
2025-01-05 11:28:04 +0300

    Removing Products section on Homepage, as it is  no longer in design

--- freshly_set/frontend/src/components/pages/homepage/Home.js
+++ freshly_set/frontend/src/components/pages/homepage/Home.js
@@ -3,7 +3,6 @@ import Nav from '../../Nav/Navbar'
 import Mission from './Mission'
 import Hero from "./Hero"
 import Services from "./Services"
-import ProductSection from "./ProductSection"
 import Reviews2 from "./Reviews2";
 import Faq from "./Faq";
 import ContactCard from "./ContactCard";
@@ -24,17 +23,15 @@ function Home() {
     <div className="">
       <Nav />
       <SubNavbar partnership={true} sections={[{title:"Metric", id: "metric"},{title:"Promotion", id: "promotion"},  {title:"FAQs", id: "faqs"}]}/>
-     <Hero id="hero" />
-     <Mission id="mission"/>
-     <Metrics id="metrics"/>
-     <Banner id="promotion"/>
-     <Services id="services" />
-     <ProductSection id="product" />
-     <Reviews2 id="reviews"/>
-     <Faq id="faqs"/>
-
-     <ContactCard id="contact"/>  
-    <FreshlyFooter />
+      <Hero id="hero" />
+      <Mission id="mission"/>
+      <Metrics id="metrics"/>
+      <Banner id="promotion"/>
+      <Services id="services" />
+      <Reviews2 id="reviews"/>
+      <Faq id="faqs"/>
+      <ContactCard id="contact"/>  
+      <FreshlyFooter />
      </div>
   )
 }
